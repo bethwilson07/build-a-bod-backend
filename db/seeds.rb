7631a0458f2f6f801400371617a5924f6da46b27
bethwilson07 <beth.ny.wlsn@gmail.com>
2018-12-04 14:01:47 -0500

    reseeded database with associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,45 @@
+
+#arms exercises
+exercise1 = Exercise.create(name: 'Dumbell Bicep Curls', description:'with a dumbbell in each hand, raise the weights until your biceps are fully contracted. Then slowly begin to lower the dumbbells back to the starting position.' , muscle_group: 'biceps', image: 'https://www.bodybuilding.com/exercises/exerciseImages/sequences/223/Male/l/223_2.jpg', video: "" )
+
+exercise2 = Exercise.create(name: 'Tricep Dips', description:'On a set of parallel bars. Raise yourself slowly until your arms are nearly completely extended. Lower yourself until your upper arm and forearm make a 90 degree angle.' , muscle_group: 'triceps', image: 'https://www.bodybuilding.com/exercises/exerciseImages/sequences/55/Male/l/55_2.jpg', video: "" )
+
+exercise3 = Exercise.create(name: 'Wrist Curls', description:'place your right forearm on top of your upper right thigh with your palm up.  curl the dumbbell as high as possible as you contract the forearms, then lower the dumbell as far as you can.' , muscle_group: 'forearms', image: 'https://www.bodybuilding.com/exercises/exerciseImages/sequences/386/Male/l/386_2.jpg', video: "" )
+
+exercise4 = Exercise.create(name: 'Dumbell Tricep Extensions', description:'Keeping your upper arms close to your head with elbows in and perpendicular to the floor, lower the resistance in a semicircular motion behind your head until your forearms touch your biceps. Return to the starting position' , muscle_group: 'tricep', image: 'https://www.bodybuilding.com/exercises/exerciseImages/sequences/345/Male/l/345_1.jpg', video: "" )
+
+exercise5 = Exercise.create(name: 'Incline Hammer Curls', description:'on a incline bench at a 45 degree angle, Allow the dumbbells to hang straight down at your side, flex at the elbows and pull the dumbell upward. Return to the starting Position' , muscle_group: 'tricep', image: 'https://www.bodybuilding.com/exercises/exerciseImages/sequences/882/Male/l/882_2.jpg', video: "" )
+
+#legs exercises
+
+exercise7 = Exercise.create(name: 'Barbell Squat', description:'stand with a barbell on the back of your shoulders on a squat rack just above shoulder level. Step away from the rack and position your legs using a shoulder-width medium stance. Lower the bar by bending the knees and sitting back with your hips as you maintain a straight posture. Raise the bar by pushing with your heels as you straighten the legs and extend the hips to go back to the starting position.' , muscle_group: 'quadriceps', image: 'https://www.bodybuilding.com/exercises/exerciseImages/sequences/64/Male/l/64_2.jpg', video: "" )
+
+exercise8 = Exercise.create(name: 'Standing Dumbbell Calf Raise
+', description:'Stand and hold two dumbbells by your sides. Place the balls of your feet on a surface elevated by 2 to 3 inches. Raise the heels off the floor as you exhale by contracting the calves, then slowly lower to the starting position.' , muscle_group: 'quadriceps', image: 'https://www.bodybuilding.com/exercises/exerciseImages/sequences/219/Male/m/219_2.jpg', video: "" )
+
+exercise9 = Exercise.create(name: 'Butt Lift Bridge', description:'Lie flat on the floor on your back with the hands by your side and your knees bent. Push with your heels, and lift your hips off the floor while keeping your back straight.' , muscle_group: 'quadriceps', image: 'https://www.bodybuilding.com/exercises/exerciseImages/sequences/219/Male/m/219_2.jpg', video: "" )
+
+exercise10 = Exercise.create(name: 'Lying Leg Curls', description:'On a leg curl machine, lie face down and curl your legs up as far as possible. Bring legs back to the initial position' , muscle_group: 'hamstrings', image: 'https://www.bodybuilding.com/exercises/exerciseImages/sequences/52/Male/l/52_2.jpg', video: "" )
+
+exercise11 = Exercise.create(name: 'Seated Leg Extensions', description:'On a leg extension machine machine, raise your legs extending until your knees are nearly locked. Bring legs back to the initial position' , muscle_group: 'quadriceps', image: 'https://www.bodybuilding.com/exercises/exerciseImages/sequences/52/Male/l/52_2.jpg', video: "" )
+
+#back exercises
+exercise12 = Exercise.create(name: 'Deadlift', description:'stand with a barbell close to your shins. Grip the bar with a double overhand grip. Squat down and drive through the front of your heels lifting the barbell. After the bar crosses the knees, drive your hips toward the bar and lower the bar back down' , muscle_group: 'back', image: 'https://www.bodybuilding.com/exercises/exerciseImages/sequences/671/Male/l/671_3.jpg', video: "" )
+
+exercise13 = Exercise.create(name: 'One Arm Dumbell Row', description:'Place one leg on top of the end of the bench, bend your torso forward from the waist until your upper body is parallel to the floor, and place your other hand on the bench for support. Pull the weight up to the side of your chest keeping your arm to the side of your body. Lower the weight back to the strting position.' , muscle_group: 'back', image: 'https://www.bodybuilding.com/exercises/exerciseImages/sequences/13/Male/l/13_2.jpg', video: "" )
+
+exercise14 = Exercise.create(name: 'Military Press', description:'while standing grab a barbell and hold it chest high with your palms facing forward. Lift the bar over your head , thn slowly lower the barbell back down to chest level.' , muscle_group: 'shoulders', image: 'https://www.bodybuilding.com/exercises/exerciseImages/sequences/370/Male/l/370_2.jpg', video: "" )
+
+exercise15 = Exercise.create(name: 'pull up', description:'Grab the pull-up bar with the palms facing forward. Pull your torso up until the bar touches your upper chest by drawing the shoulders and the upper arms down and back. Lower yourself back to the atarting position.' , muscle_group: 'shoulders', image: 'https://www.bodybuilding.com/exercises/exerciseImages/sequences/370/Male/l/370_2.jpg', video: "" )
+
 #abs exercises
 
-crunches = Exercise.create(name: 'crunch', description: 'Lie on your back with your knees bent and feet flat on the floor, hip-width apart. Place your hands behind your head so your thumbs are behind your ears. Hold for a moment at the
+exercise16 = Exercise.create(name: 'crunch', description: 'Lie on your back with your knees bent and feet flat on the floor, hip-width apart. Place your hands behind your head so your thumbs are behind your ears. Hold for a moment at the
   top of the movement and then lower slowly back down.', muscle_group: 'abs',
   image:'https://cdn-xi3mbccdkztvoept8hl.netdna-ssl.com/wp-content/uploads/watermarked/Sit_up_Crunch_F_WorkoutLabs.png',
   video: "" )
 
-deadBug = Exercise.create(name: 'dead bug', description: 'Lie face-up with your
+exercise17 = Exercise.create(name: 'dead bug', description: 'Lie face-up with your
   arms extended toward the ceiling, directly over your shoulders, and knees bent
   90 degrees over hips, calves parallel to floor. Keeping your shoulders
   down and feet flexed, engage your core and extend your right arm and left leg
@@ -13,7 +47,7 @@ deadBug = Exercise.create(name: 'dead bug', description: 'Lie face-up with your
   image:'https://s3.amazonaws.com/brightcove-still-assets/3924207686001.jpg',
   video: "" )
 
-russianTwist = Exercise.create(name: 'russian twist', description: 'Sit with
+exercise18 = Exercise.create(name: 'russian twist', description: 'Sit with
   your torso leaning back at a 45-degree angle, knees bent, and your feet either
   on the floor or elevated a few inches. In most variations you hold something
   in front of your chest, usually a medicine ball or weight plate. From that
@@ -21,37 +55,37 @@ russianTwist = Exercise.create(name: 'russian twist', description: 'Sit with
   image:'https://image.shutterstock.com/image-photo/russian-twist-young-man-doing-260nw-570574084.jpg',
   video: "" )
 
-plank = Exercise.create(name: 'plank', description: 'hold a push-up initial
+exercise19 = Exercise.create(name: 'plank', description: 'hold a push-up initial
   position, with your weight on forearms, elbows, and toes.', muscle_group: 'abs',
   image:'https://cdn.lifehack.org/wp-content/uploads/2017/08/11072609/plank.jpg',
   video: "" )
 
-bicycles = Exercise.create(name: 'bicycle crunch', description: 'Lie on your back
+exercise20 = Exercise.create(name: 'bicycle crunch', description: 'Lie on your back
   with your knees bent and feet flat on the floor. Place your hands behind head.
   Raise your shoulder blades off the ground and lift your legs off the floor and
   bend knees are to 90 degrees. Then slowly pedal them in the air, alternating as
   if riding a bicycle.', muscle_group: 'abs',
   image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnaFGFQt90l8k0XeYhvab6my6NPwPhnd-NCWRF-_8GPVIZIUB1',
   video: "" )
-superMan = Exercise.create(name: 'superman', description: 'Simultaneously raise
+exercise21 = Exercise.create(name: 'superman', description: 'Simultaneously raise
   your arms, legs, and chest off of the floor and hold this contraction for 2 seconds.
   Squeeze your lower back to get the best results from this exercise.', muscle_group: 'abs',
   image:'https://media1.popsugar-assets.com/files/thumbor/i2bQ2NxyqViybX5716liUZBqBkE/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2015/08/24/881/n/1922398/13a573f1_Superman.jpg',
   video: "" )
-legLifts = Exercise.create(name: 'leg lift', description: 'Lay down with your
+exercise22 = Exercise.create(name: 'leg lift', description: 'Lay down with your
   hands to your sides for support. Raise your legs so they are off the ground,
   keeping the abdominals tight. Slowly raise your legs 90 degrees, contracting
   the abdominal muscles. Slowly return to starting position. Do not touch the
   floor with your feet.', muscle_group: 'abs',
   image:'https://image.shutterstock.com/image-photo/woman-doing-leg-lifts-strengthen-260nw-124035805.jpg',
   video: "" )
-sidePlank = Exercise.create(name: 'side plank v-ups (R)', description: 'Lie
+exercise23 = Exercise.create(name: 'side plank v-ups (R)', description: 'Lie
 on side with the legs stacked on top of one another then prop the body up on
 the hand or elbow while keeping the feet stacked.', muscle_group: 'abs',
 image: 'https://bodybuilding-wizard.com/wp-content/uploads/2015/05/side-plank.jpg',
 video: "" )
 
-flutterKicks = Exercise.create(name: 'flutter kicks', description: 'lie on your
+exercise24 = Exercise.create(name: 'flutter kicks', description: 'lie on your
   back with hands under your glutes. Hold your feet above the ground and alternate
   kicking with your legs. Do not let your legs touch the ground.', muscle_group: 'abs',
   image: 'https://www.cdn.spotebi.com/wp-content/uploads/2014/10/flutter-kicks-exercise-illustration.jpg',
@@ -96,36 +130,16 @@ chestPress = Exercise.create(name: 'free weight chest press', description: 'Lie
   muscle_group: 'chest', image:'https://dumbbell-exercises.com/wp/wp-content/uploads/2017/01/1.gif',
   video: "" )
 
-#arms exercises
-exercise1 = Exercise.create(name: 'Dumbell Bicep Curls', description:'with a dumbbell in each hand, raise the weights until your biceps are fully contracted. Then slowly begin to lower the dumbbells back to the starting position.' , muscle_group: 'biceps', image: 'https://www.bodybuilding.com/exercises/exerciseImages/sequences/223/Male/l/223_2.jpg', video: "" )
 
-exercise2 = Exercise.create(name: 'Tricep Dips', description:'On a set of parallel bars. Raise yourself slowly until your arms are nearly completely extended. Lower yourself until your upper arm and forearm make a 90 degree angle.' , muscle_group: 'triceps', image: 'https://www.bodybuilding.com/exercises/exerciseImages/sequences/55/Male/l/55_2.jpg', video: "" )
+#workouts
 
-exercise3 = Exercise.create(name: 'Wrist Curls', description:'place your right forearm on top of your upper right thigh with your palm up.  curl the dumbbell as high as possible as you contract the forearms, then lower the dumbell as far as you can.' , muscle_group: 'forearms', image: 'https://www.bodybuilding.com/exercises/exerciseImages/sequences/386/Male/l/386_2.jpg', video: "" )
+legWorkout = Workout.create(name: "Leg Workout", day: "Monday", muscle_group: "Legs", duration: 45)
+armWorkout = Workout.create(name: "Arm Workout", day: "Tuesday", muscle_group: "Arms", duration: 45)
 
-exercise4 = Exercise.create(name: 'Dumbell Tricep Extensions', description:'Keeping your upper arms close to your head with elbows in and perpendicular to the floor, lower the resistance in a semicircular motion behind your head until your forearms touch your biceps. Return to the starting position' , muscle_group: 'tricep', image: 'https://www.bodybuilding.com/exercises/exerciseImages/sequences/345/Male/l/345_1.jpg', video: "" )
+WorkoutExercise.create(workout_id: 1, exercise_id: 7)
+WorkoutExercise.create(workout_id: 1, exercise_id: 10)
+WorkoutExercise.create(workout_id: 1, exercise_id: 11)
 
-exercise5 = Exercise.create(name: 'Incline Hammer Curls', description:'on a incline bench at a 45 degree angle, Allow the dumbbells to hang straight down at your side, flex at the elbows and pull the dumbell upward. Return to the starting Position' , muscle_group: 'tricep', image: 'https://www.bodybuilding.com/exercises/exerciseImages/sequences/882/Male/l/882_2.jpg', video: "" )
-
-#legs exercises
-
-
-exercise7 = Exercise.create(name: 'Barbell Squat', description:'stand with a barbell on the back of your shoulders on a squat rack just above shoulder level. Step away from the rack and position your legs using a shoulder-width medium stance. Lower the bar by bending the knees and sitting back with your hips as you maintain a straight posture. Raise the bar by pushing with your heels as you straighten the legs and extend the hips to go back to the starting position.' , muscle_group: 'quadriceps', image: 'https://www.bodybuilding.com/exercises/exerciseImages/sequences/64/Male/l/64_2.jpg', video: "" )
-
-exercise8 = Exercise.create(name: 'Standing Dumbbell Calf Raise
-', description:'Stand and hold two dumbbells by your sides. Place the balls of your feet on a surface elevated by 2 to 3 inches. Raise the heels off the floor as you exhale by contracting the calves, then slowly lower to the starting position.' , muscle_group: 'quadriceps', image: 'https://www.bodybuilding.com/exercises/exerciseImages/sequences/219/Male/m/219_2.jpg', video: "" )
-
-exercise9 = Exercise.create(name: 'Butt Lift Bridge', description:'Lie flat on the floor on your back with the hands by your side and your knees bent. Push with your heels, and lift your hips off the floor while keeping your back straight.' , muscle_group: 'quadriceps', image: 'https://www.bodybuilding.com/exercises/exerciseImages/sequences/219/Male/m/219_2.jpg', video: "" )
-
-exercise10 = Exercise.create(name: 'Lying Leg Curls', description:'On a leg curl machine, lie face down and curl your legs up as far as possible. Bring legs back to the initial position' , muscle_group: 'hamstrings', image: 'https://www.bodybuilding.com/exercises/exerciseImages/sequences/52/Male/l/52_2.jpg', video: "" )
-
-exercise11 = Exercise.create(name: 'Seated Leg Extensions', description:'On a leg extension machine machine, raise your legs extending until your knees are nearly locked. Bring legs back to the initial position' , muscle_group: 'quadriceps', image: 'https://www.bodybuilding.com/exercises/exerciseImages/sequences/52/Male/l/52_2.jpg', video: "" )
-
-#back exercises
-exercise12 = Exercise.create(name: 'Deadlift', description:'stand with a barbell close to your shins. Grip the bar with a double overhand grip. Squat down and drive through the front of your heels lifting the barbell. After the bar crosses the knees, drive your hips toward the bar and lower the bar back down' , muscle_group: 'back', image: 'https://www.bodybuilding.com/exercises/exerciseImages/sequences/671/Male/l/671_3.jpg', video: "" )
-
-exercise13 = Exercise.create(name: 'One Arm Dumbell Row', description:'Place one leg on top of the end of the bench, bend your torso forward from the waist until your upper body is parallel to the floor, and place your other hand on the bench for support. Pull the weight up to the side of your chest keeping your arm to the side of your body. Lower the weight back to the strting position.' , muscle_group: 'back', image: 'https://www.bodybuilding.com/exercises/exerciseImages/sequences/13/Male/l/13_2.jpg', video: "" )
-
-exercise14 = Exercise.create(name: 'Military Press', description:'while standing grab a barbell and hold it chest high with your palms facing forward. Lift the bar over your head , thn slowly lower the barbell back down to chest level.' , muscle_group: 'shoulders', image: 'https://www.bodybuilding.com/exercises/exerciseImages/sequences/370/Male/l/370_2.jpg', video: "" )
-
-exercise15 = Exercise.create(name: 'pull up', description:'Grab the pull-up bar with the palms facing forward. Pull your torso up until the bar touches your upper chest by drawing the shoulders and the upper arms down and back. Lower yourself back to the atarting position.' , muscle_group: 'shoulders', image: 'https://www.bodybuilding.com/exercises/exerciseImages/sequences/370/Male/l/370_2.jpg', video: "" )
+WorkoutExercise.create(workout_id: 2, exercise_id: 1)
+WorkoutExercise.create(workout_id: 2, exercise_id: 2)
+WorkoutExercise.create(workout_id: 2, exercise_id: 3)
