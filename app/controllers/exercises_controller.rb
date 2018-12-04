4e66cd13d5179d88cd79c66ddab23ccf4c3a0a82
bethwilson07 <beth.ny.wlsn@gmail.com>
2018-12-04 14:37:37 -0500

    build exercise controller

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,2 +1,29 @@
 class ExercisesController < ApplicationController
+
+  def index
+    render json: Exercise.all
+  end
+
+  def show
+    render json: Exercise.find(params[:id])
+  end
+
+  def create
+    render json: Exercise.create(exercise_params)
+  end
+
+  def update
+    Exercise.find(params[:id]).update(exercise_params)
+    render json: Exercise.find(params[:id])
+  end
+
+  def destroy
+    render json: Exercise.find(params[:id]).destroy
+  end
+
+  private
+
+    def exercise_params
+      params.require(:exercise).permit(:id, :name, :description, :muscle_group, :image, :video)
+    end
 end
