f269475d4bad3881850f6f30c127ac65de597af0
Drewisatlas <drewisatlas@gmail.com>
2018-12-04 14:40:19 -0500

    added controller actions for workouts

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,29 @@
 class WorkoutsController < ApplicationController
+
+  def index
+    render json: Workout.all
+  end
+
+  def show
+    render json: Workout.find(params[:id])
+  end
+
+  def create
+    render json: Workout.create(workout_params)
+  end
+
+  def update
+    Workout.find(params[:id]).update(workout_params)
+    render json: Workout.find(params[:id])
+  end
+
+  def destroy
+    render json: Workout.find(params[:id]).destroy
+  end
+
+  private
+  def workout_params
+    params.require(:workout).permit(:id, :name, :day, :muscle_group, :duration)
+  end
+
 end
