24b005db473def92ec910aa3bb101fda1486b5b5
Beth Wilson <bethwilson@Beths-MacBook-Pro-2.local>
2021-06-10 18:52:17 -0400

    upgrading ruby version and rails version

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  resources :workouts, :exercises
-  resources :workout_exercises, only: [:index, :show, :create, :update, :destroy]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
